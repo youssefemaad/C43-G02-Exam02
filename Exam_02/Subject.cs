d14e563473be5af36bb96fc3f4d0d1f973996ba5
youssefemaad <eyoussef228@gmail.com>
2025-02-05 01:09:09 +0200

    new

--- Exam_02/Subject.cs
+++ Exam_02/Subject.cs
@@ -18,7 +18,6 @@ namespace Exam_02
             SubjectName = subjectName;
         }
 
-        // make a methoud that create a new exam
         public void CreateExam()
         {
             Console.Write("Please Enter the type of exam you want to create (1 for Practical and 2 for Final): ");
@@ -30,30 +29,15 @@ namespace Exam_02
 
             if (examType == 1)
             {
-                Exam = new PracticalExam(time,numQuestions);
-            }
-            else
-            {
-                Exam = new FinalExam(time,numQuestions);
-            }
+                Exam = new PracticalExam(time, numQuestions);
 
-            for (int i = 0; i < numQuestions; i++)
-            {
-                Console.Write($"Please choose the Type of Question Number ({i + 1}) (1 for T/F || 2 for MCQ): ");
-                int qType = int.Parse(Console.ReadLine());
-                Console.Write("Please Enter the Body of Question: ");
-                string body = Console.ReadLine();
-                Console.Write("Please Enter The Marks of Question: ");
-                int mark = int.Parse(Console.ReadLine());
+                for (int i = 0; i < numQuestions; i++)
+                {
+                    Console.Write($"Please Enter the Body of Question ({i + 1}): ");
+                    string body = Console.ReadLine();
+                    Console.Write("Please Enter The Marks of Question: ");
+                    int mark = int.Parse(Console.ReadLine());
 
-                if (qType == 1)
-                {
-                    Console.Write("Please Enter the Correct Answer (1 for True | 2 for False): ");
-                    int correctAnswer = int.Parse(Console.ReadLine());
-                    Exam.AddQuestion(new TrueFalse("True/False Questions", body, mark, correctAnswer));
-                }
-                else
-                {
                     List<Answer> answers = new List<Answer>();
                     for (int j = 0; j < 4; j++)
                     {
@@ -65,10 +49,46 @@ namespace Exam_02
                     Console.Write("Please Enter the Correct Answer Id: ");
                     int correctAnswerId = int.Parse(Console.ReadLine());
                     Exam.AddQuestion(new McqQuestion("MCQ Questions", body, mark, answers, correctAnswerId));
-                }
 
+                }
             }
 
+            else
+            {
+                Exam = new FinalExam(time, numQuestions);
+
+                for (int i = 0; i < numQuestions; i++)
+                {
+                    Console.Write($"Please choose the Type of Question Number ({i + 1}) (1 for T/F || 2 for MCQ): ");
+                    int qType = int.Parse(Console.ReadLine());
+                    Console.Write("Please Enter the Body of Question: ");
+                    string body = Console.ReadLine();
+                    Console.Write("Please Enter The Marks of Question: ");
+                    int mark = int.Parse(Console.ReadLine());
+
+                    if (qType == 1)
+                    {
+                        Console.Write("Please Enter the Correct Answer (1 for True | 2 for False): ");
+                        int correctAnswer = int.Parse(Console.ReadLine());
+                        Exam.AddQuestion(new TrueFalse("True/False Questions", body, mark, correctAnswer));
+                    }
+                    else
+                    {
+                        List<Answer> answers = new List<Answer>();
+                        for (int j = 0; j < 4; j++)
+                        {
+                            Console.Write($"Please Enter the Answer Number ({j + 1}): ");
+                            string ans = Console.ReadLine();
+                            answers.Add(new Answer(j + 1, ans));
+                        }
+
+                        Console.Write("Please Enter the Correct Answer Id: ");
+                        int correctAnswerId = int.Parse(Console.ReadLine());
+                        Exam.AddQuestion(new McqQuestion("MCQ Questions", body, mark, answers, correctAnswerId));
+                    }
+
+                }
+            }
 
         }
     }
